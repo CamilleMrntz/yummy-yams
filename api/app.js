f5f4bdf59285f20c57e1f443fcb35fa17f84b927
camilleMrntz <camille.1@hotmail.fr>
2024-04-13 18:24:56 +0200

    registration added

--- api/app.js
+++ api/app.js
@@ -1,58 +1,86 @@
-import express from "express";
-import mongoose from "mongoose";
-import Pastry from "./models/pastries.mjs";
+import express from "express"
+import mongoose from "mongoose"
+import Pastry from "./models/pastries.mjs"
+import cors from "cors"
+import User from "./models/users.mjs"
 
-const app = express();
-const port = 3001;
+const app = express()
+const port = 3001
 
-mongoose.connect('mongodb://mongo:27017/yams_db', {  // mongo:27017 pour lancer avec docker  // localhost:27017 pour lancer en local
+app.use(cors())
+app.use(express.json())
+
+
+mongoose.connect('mongodb://localhost:27017/yams_db', {  // mongo:27017 pour lancer avec docker  // localhost:27017 pour lancer en local
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
 .then(() => {
-  console.log('Connexion à MongoDB réussie');
+  console.log('Connexion à MongoDB réussie')
 })
 .catch((error) => {
-  console.error('Erreur de connexion à MongoDB :', error);
-});
+  console.error('Erreur de connexion à MongoDB :', error)
+})
 
-// 1. Ajoutez une route d'accueil affichant un objet JSON contenant votre nom/prénom/age
-app.get("/home", (req, res) => {
-  res.send({
-    nom: 'CLERYYY',
-    prenom: 'Jean-Marie',
-    age: 35,
-  });
-});
+
+// REGISTRATION
+app.post('/registration', async (req, res) => {
+  console.log(req.body)
+  try {
+    const user = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    })
+    res.json({ status: 'ok'})
+  } catch (error) {
+    res.json({ status: 'error', error: 'Duplicated email' })
+  }
+})
+
+// LOGIN
+app.post('/login', async (req, res) => {
+  const user = await User.findOne({
+    email: req.body.email,
+    password: req.body.password,
+  })
+  if (user) {
+    res.json({ status: 'ok', user: true })
+  } else {
+    res.json({ status: 'error', user: false })
+  }
+  
+  
+})
+
+
 
 // Return the json
 app.get("/pastries", async (req, res) => {
     try {
-        const pastries = await Pastry.find();
-        res.json(pastries);
+        const pastries = await Pastry.find()
+        res.json(pastries)
     } catch (err) {
-        console.error("Erreur :", err);
-        res.status(500).json({ message: "Une erreur s'est produite lors de la récupération des pâtisseries" });
+        console.error("Erreur :", err)
+        res.status(500).json({ message: "Une erreur s'est produite lors de la récupération des pâtisseries" })
     }
 })
 
 // Return the count of pastries left
 app.get("/pastries-left", async (req, res) => {
     try {
-        const pastries = await Pastry.find();
-        var pastriesLeft = 0;
+        const pastries = await Pastry.find()
+        var pastriesLeft = 0
         pastries.forEach(pastry => {
-            pastriesLeft += pastry.stock;
+            pastriesLeft += pastry.stock
         });
-        console.log(pastriesLeft);
-        res.json(pastriesLeft.toString());
+        console.log(pastriesLeft)
+        res.json(pastriesLeft.toString())
     } catch (err) {
-        console.error("Erreur :", err);
-        res.status(500).json({ message: "Une erreur s'est produite lors de la récupération des pâtisseries" });
+        console.error("Erreur :", err)
+        res.status(500).json({ message: "Une erreur s'est produite lors de la récupération des pâtisseries" })
     }
 })
-
-
 
 
 
--- api/models/pastries.mjs
+++ api/models/pastries.mjs
@@ -1,11 +1,14 @@
 import mongoose from "mongoose";
 
-const pastrySchema = new mongoose.Schema({
-  name: String,
-  image: String,
-  stock: Number,
-  quantityWon: Number 
-});
+const pastrySchema = new mongoose.Schema(
+  {
+    name: String,
+    image: String,
+    stock: Number,
+    quantityWon: Number 
+  },
+  { collection: 'pastries' }
+)
 
 const Pastry = mongoose.model('pastries', pastrySchema);
 
