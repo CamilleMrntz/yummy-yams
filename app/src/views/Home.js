d313a3a0f377807576e13082b110274b9576eb15
camilleMrntz <camille.1@hotmail.fr>
2024-04-21 21:06:05 +0200

    images moved to /public so they render in browser

--- app/src/views/Home.js
+++ app/src/views/Home.js
@@ -1,6 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 import React, { useState, useEffect } from "react";
-import styles from './../css/home.module.css'
+import styles from './../css/home.module.css';
 
 function Home() {
 
@@ -24,36 +24,6 @@ function Home() {
         )
     }, []);
 
-    function slide() {
-        const caroussel = document.querySelector('.caroussel')
-        const caroussel_1 = document.querySelector('.img_caroussel')
-        
-        if (caroussel) {
-            caroussel.appendChild(caroussel_1)
-        } else {
-            console.log('no')
-        }
-    }
-
-    slide()
-
-    // useEffect(() => {
-    //     const intervalId = setInterval(() => {
-    //       const caroussel = document.querySelector('.img_caroussel');
-    //       if (caroussel) {
-    //         const firstImage = caroussel.querySelector('img');
-    
-    //         // Déplacer la première image à la fin du carrousel
-    //         caroussel.appendChild(firstImage.cloneNode(true));
-    //         caroussel.removeChild(firstImage);
-    //       } else {
-    //         console.log('no')
-    //       }
-    //     }, 10000); // Modifier cette valeur pour ajuster la vitesse du carrousel
-    
-    //     return () => clearInterval(intervalId);
-    //   }, []);
-
     return(
         <div>
             <h1>Yummy Yams</h1>
@@ -62,13 +32,11 @@ function Home() {
             <button onClick={register}>Créer mon compte</button>
             <button onClick={login}>Se connecter</button>
             {pictures.length > 0 && (
-            <section className={styles.caroussel}>
-                <div className={styles.img_caroussel}>
-                    {pictures.map((item) => (
-                        <img key={item} src={`./../images/${item}`} alt={item} />
-                    ))}
-                </div>
-            </section>
+            <div className={styles.img_caroussel}>
+                {pictures.map((item) => (
+                    <img key={item} src={`/images/${item}`} alt={item} />
+                ))}
+            </div>
             )}
         </div>
     );
