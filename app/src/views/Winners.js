06ddfd822428908b8eb294bc56889851e18ecb90
camilleMrntz <camille.1@hotmail.fr>
2024-05-03 03:15:05 +0200

    winners page done

--- app/src/views/Winners.js
+++ app/src/views/Winners.js
@@ -15,16 +15,43 @@ function Winners() {
     )
 
 
-
   }, []);
+
+  function formatDate(dateOfwinning) {
+    const dateObj = new Date(dateOfwinning);
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    const date = dateObj.toLocaleDateString('fr-FR', options);
+
+    return date
+  }
+
+  function formatTime(dateOfwinning) {
+    const dateObj = new Date(dateOfwinning);
+    const time = dateObj.toTimeString().slice(0, 8);
+
+    return time
+  }
 
 
   return (
     <div className={styles.main}>
+      <h1>🏆</h1>
       {winners.length > 0 && (
-        <div className={styles.winner}>
+        <div className={styles.winners}>
+            <div className={styles.winner_card + ' ' + styles.winner_title}>
+              <p>gagnant</p>
+              <p>date</p>
+              <p>heure</p>
+              <p>nombre</p>
+            </div>
           {winners.map((item) => (
-            <p key={item}>{item.userName}</p>
+            <div className={styles.winner_card + ' ' + styles.winner_card_only}>
+              <p key={item}>{item.userName}</p>
+              <p key={item}>{formatDate(item.date)}</p>
+              <p key={item}>{formatTime(item.date)}</p>
+              <p key={item}>{item.numberOfPastriesWon}</p>
+              {/* <p key={item}>{item.pastries.name}</p> */}
+            </div>
           ))}
         </div>
       )}
